5e2c36518d2a7a77a3ba9db4f9be498013670af6
James Beasley <jbeasley@alumni.cmu.edu>
2017-02-28 02:07:31 -0600

    n/a

--- sdcp4/src/pipeline.py
+++ sdcp4/src/pipeline.py
@@ -11,10 +11,11 @@ from moviepy.editor import VideoFileClip
 
 #run the pipeline on the provided video
 def execute_pipeline(calibration_object_points, calibration_image_points):
+    process_frame_function_handle = process_frame(calibration_object_points, calibration_image_points)
     video_handle = VideoFileClip("test_videos/project_video.mp4")
-    video_handle = video_handle.fl_image(process_frame)
-    video_handle.write_videofile("output_videos/processed_project_video.mp4", audio=False)
+    image_handle = video_handle.fl_image(process_frame_function_handle)
+    image_handle.write_videofile("output_videos/processed_project_video.mp4", audio=False)
 
 #run the pipeline on the provided video
-def process_frame():
+def process_frame(calibration_object_points, calibration_image_points):
     return None
